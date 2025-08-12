bf9b78481764957f43c5d77a31876c561e126b46
Jaswanth Reddy <126372148+Jaswanth534@users.noreply.github.com>
2025-08-12 22:59:07 +0530

    first-non-repeating-char.js

---
+++ DSA-JavaScript/Linked-List/first-non-repeating-char.js
@@ -0,0 +1,22 @@
+// Example 1:
+
+// Input: s = "leetcode"
+
+// Output: 0
+
+
+let s = "leetcode"
+
+function firstUniqueChar(s){
+
+    let map = {}
+    for(let char of s){
+        map[char] ? map[char]++ : map[char] = 1;
+    }
+    for(let i = 0; i < s.length; i++){
+        if(map[s[i]] === 1) return i
+    }
+    return -1
+}
+
+console.log(firstUniqueChar(s))
